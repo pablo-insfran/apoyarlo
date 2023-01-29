4ec27d9fcfaec1ea257118ccaf99144a77bcb455
pablo-insfran <pablo98dani@gmail.com>
2023-01-29 16:21:25 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ function App() {
     <div className="App">
       <PersonCard fristName="Doe" lastName="Jane" age={45} hairColor="Black" />
       <PersonCard fristName="John" lastName="Smith" age={88} hairColor="Brown" />
+      <PersonCard fristName="Millard" lastName="Fillmore" age={50} hairColor="Brown" />
+      <PersonCard fristName="Maria" lastName="Smith" age={62} hairColor="Brown" />
     </div>
   );
 }
